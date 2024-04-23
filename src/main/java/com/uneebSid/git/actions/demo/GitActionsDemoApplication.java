ba536cdf62783feb397f3f742fa72263ca7a086e
UneebSidd <=uu1997@gmail.com>
2024-04-23 14:56:55 -0400

    initial commit

---
+++ src/main/java/com/uneebSid/git/actions/demo/GitActionsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.uneebSid.git.actions.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitActionsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uneebSid/git/actions/demo/GitActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uneebSid.git.actions.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
